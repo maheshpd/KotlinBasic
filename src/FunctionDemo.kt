43a4a4e0eb5ee0ee499ee46418d568e4c08f798c
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-09-25 18:36:18 +0530

    If Array.....

--- src/FunctionDemo.kt
+++ src/FunctionDemo.kt
@@ -1,20 +1,69 @@
 fun main() {
 
     //Using Standard Library Function
-    var num = 25
-    println("Sqaure root of $num is: ${Math.sqrt(num.toDouble())}")
+//    var num = 25
+//    println("Sqaure root of $num is: ${Math.sqrt(num.toDouble())}")
 
     //User- defined function
-    sayHello()
+//   var result =  sayHello(10,"Hetvi")
+//    println(result)
+
+//    var addition = sum(2,3) //5
+//    addition  = 2
+//
+//    println(addition)
+//
+////    val addtion = sum(2,3)
+//    println(sum(2,3))
+//    var principleAmount = sum(3,2)
+//    principleAmount +=2
+//
+//    println(principleAmount)
+
+//    val addi = sum(2,3)
+//    println(addi)
+//    val names = GetName("Hetvi")
+//    println(names)
+
+    println(10+20)
+
+    println(addtion(2, 3))  //5
+    println(sub(3, 1)) //2
+    println(mul(2, 2)) //4
+
+//    /addtion
+// mul
+    //sub
+    //division
+
+    sum(2,3)
+
 }
 
-
-fun sayHello() {
-    println("Welcome Hetvi")
+//Non-return type
+fun sayHello(n: Int, name: String) {
+    println("Number is $n and name is $name ")
 }
 
-fun sum(a:Int,b:Int): Int {
+//2nd example
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
 
-    return a+b
+//String function
+fun GetName(name: String): String {
+    return name
+}
 
+fun addtion(n1: Int, n2: Int): Int {
+    return n1 + n2
+
+}
+
+fun sub(n1: Int, n2: Int): Int {
+    return n1 - n2
+}
+
+fun mul(n1: Int, n2: Int): Int {
+    return n1 * n2
 }
--- src/RecursionClass.kt
+++ src/RecursionClass.kt
@@ -1,16 +1,21 @@
 fun main() {
     print("Enter a positive integer number: ")
     val number: Int = Integer.valueOf(readLine())
+
     val factorial = fact(number)
+
     println("Factorial of $number = $factorial")
 }
 
-//recursive function
+// recursive function
 fun fact(num: Int): Int {
+
     return if (num == 1) {
         num
     } else {
+
         //function fact() calling itself
+        // recursive function
         num*fact(num-1)
     }
 }
